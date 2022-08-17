0e2f9e7533f622b1a4dd3333c44c381a31bb2c5c
Samir Kumar <46542096+krsamir@users.noreply.github.com>
2022-08-17 12:06:26 +0530

    Filtering by multiple column

--- index.js
+++ index.js
@@ -5,7 +5,18 @@ config();
 
 const typeDefs = gql`
   type Query {
-    sessions: [Session]
+    sessions(
+      id: ID
+      title: String
+      description: String
+      startsAt: String
+      endsAt: String
+      room: String
+      day: String
+      format: String
+      track: String
+      level: String
+    ): [Session]
     sessionById(id: ID): Session
   }
   type Session {
@@ -28,7 +39,7 @@ const typeDefs = gql`
 const resolvers = {
   Query: {
     sessions: (parent, args, { dataSources }, info) =>
-      dataSources.sessionAPI.getSessions(),
+      dataSources.sessionAPI.getSessions(args),
     sessionById: (parent, { id }, { dataSources }, info) => {
       return dataSources.sessionAPI.getSessionById(id);
     },
